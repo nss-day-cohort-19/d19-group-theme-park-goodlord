c47f8ce567fb4ff2776ee0c2e2e0762531ecee76
Andrew Rock <Andrew Rock>
2017-05-15 13:35:01 -0500

    park info load successful

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,3 +4,12 @@
 console.log("is main loading?");
 
 require("bootstrap");
+let header = require("./header.js");
+let parkInfo;
+
+header.getInfo()
+	.then((data) => {
+		parkInfo = data;
+		console.log(parkInfo);
+	});
+
