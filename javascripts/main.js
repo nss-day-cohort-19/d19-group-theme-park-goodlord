f3300d1866b624836fe45a008525b57e763f5e9b
Tamela Lerma <tlerma83@gmail.com>
2017-05-15 15:50:32 -0500

    added all loads and promises

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,14 +5,36 @@ console.log("is main loading?");
 
 require("bootstrap");
 let header = require("./header.js");
+let types = require("./types.js");
 let areas = require("./areas.js");
+let attractions = require("./attractions.js");
 let parkInfo;
+let typesInfo;
+let areasInfo;
+let attractionsInfo;
 
 header.getInfo()
 	.then(
     (data) => {
-		parkInfo = data;
-		console.log(parkInfo);
-        retrun areas.getAreas();
-	}).then()
+            parkInfo = data;
+            console.log(parkInfo);
+            return types.getTypes();
+	}).then(
+        (data) => {
+            typesInfo = data;
+            console.log(typesInfo);
+            return areas.getAreas();
+    }).then(
+        (data) => {
+            areasInfo = data;
+            console.log(areasInfo);
+            return attractions.getAttractions();
+    }).then(
+        (data) => {
+            attractionsInfo = data;
+            console.log(attractionsInfo);
+            console.log("anything happening");
+    }).catch(function(error){
+        console.log(error);
+    });
 
