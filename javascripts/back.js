5af93120e8b34536b3fa22479b0c439765c19b1a
ollieOsh <sosinusi@gmail.com>
2017-05-17 14:33:34 -0500

    i think this should work. button needs to have class hidden in index. map function in events.js must remove class hidden

--- javascripts/back.js
+++ javascripts/back.js
@@ -3,6 +3,9 @@
 $('#back').click(function(event) {
 	let current = $('div.container-fluid').not('.hidden');
 
+	if(current[0] == $('#typeScreen')[0]){
+		$('#back').addClass('hidden');
+	}
 	current.addClass('hidden');
 	current.prev().removeClass('hidden');
 });
